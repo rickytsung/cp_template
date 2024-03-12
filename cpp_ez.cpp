d4e82068c5df2ce90633e4b55d579987970cab53
rickytsung <76990058+rickytsung@users.noreply.github.com>
2024-03-12 10:47:37 +0800

    Create cpp_ez.cpp

---
+++ cpp_ez.cpp
@@ -0,0 +1,23 @@
+//Author : rickytsung
+//
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+#include <bits/stdc++.h>
+#define i64 long long
+#define i128 __int128
+#define IOS ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define pii pair<int, int>
+#define f first
+#define s second
+#define pb(x) push_back(x)
+using namespace std;
+mt19937 rng(114514);
+const int mod1 = 998244353;
+int rnd(int l, int r) {
+    return uniform_int_distribution<int>(l, r)(rng);
+}
+
+int main() {
+    IOS;
+    
+}
