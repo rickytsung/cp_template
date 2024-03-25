29064337d524c2e482452972cd23bec6ff61dd1c
rickytsung <76990058+rickytsung@users.noreply.github.com>
2024-03-25 19:21:11 +0800

    Update cpp_ez.cpp

--- cpp_ez.cpp
+++ cpp_ez.cpp
@@ -14,6 +14,31 @@
 using namespace std;
 mt19937 rng(114514);
 const int mod1 = 998244353;
+
+//i128 io template
+inline void read(i128 &n){
+    i128 x = 0,f = 1;
+    char ch = getchar();
+    while(ch < '0'||ch > '9'){
+        if(ch == '-') f = -1;
+        ch = getchar();
+    }
+    while(ch>='0'&&ch<='9'){
+        x = (x<<1)+(x<<3)+(ch^48);
+        ch = getchar();
+    }
+    n = x * f;
+}
+
+inline void print(i128 n){
+    if(n<0){
+        putchar('-');
+        n*=-1;
+    }
+    if(n>9) print(n/10);
+    putchar(n % 10 + '0');
+}
+
 int rnd(int l, int r) {
     return uniform_int_distribution<int>(l, r)(rng);
 }
